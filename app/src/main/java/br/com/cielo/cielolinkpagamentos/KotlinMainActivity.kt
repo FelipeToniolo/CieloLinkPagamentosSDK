38b2ef49fde7eed3a01bee1a487ba60ea29bdcac
ftoniolo <ftoniolo@braspag.com.br>
2019-09-23 14:03:41 -0300

    exemplo kotlin

--- app/src/main/java/br/com/cielo/cielolinkpagamentos/KotlinMainActivity.kt
+++ app/src/main/java/br/com/cielo/cielolinkpagamentos/KotlinMainActivity.kt
@@ -9,28 +9,28 @@ import br.com.cielo.librarycielolinkpagamentos.models.paymentlink.SaleType
 import br.com.cielo.librarycielolinkpagamentos.service.Environment
 import com.cielo.cielopaymentlinkclient.models.paymentlink.recurrent.RecurrentInterval
 import com.cielo.cielopaymentlinkclient.models.paymentlink.shipping.ShippingType
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.kotlin_activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class KotlinMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.kotlin_activity_main)
 
         val paymentsLink = CieloPaymentsLinkClient(
             environment= Environment.SANDBOX,
-            clientID = "df66638b-3ef4-421f-a18e-e20dea38d97d",
-            clientSecret = "q13XZ48haFg4EhAS2cjcoyX7OzRECYysY6T9TJLmKNM="
+            clientID = "SEUCLIENTID",
+            clientSecret = "SEUCLIENTSECRET"
         )
         val parameters = CieloPaymentsLinkParameters(
             "Pedido", "4000", SaleType.DIGITAL, ShippingType.WITHOUTSHIPPING,
             "teste", "1000000000", recurrentInterval = RecurrentInterval.MONTHLY
         )
 
-//        val parameters2 = CieloPaymentsLinkParameters(
-//            "Pedido", "10000", SaleType.DIGITAL, ShippingType.WITHOUTSHIPPING,
-//            "teste", "1000000000"
-//        )
+        val parameters2 = CieloPaymentsLinkParameters(
+            "Pedido", "10000", SaleType.DIGITAL, ShippingType.WITHOUTSHIPPING,
+            "teste", "1000000000"
+        )
 
 
         paymentsLink.generateLink(parameters, object :
@@ -44,16 +44,16 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-//        paymentsLink.generateLink(parameters2, object :
-//            CieloPaymentsLinkCallbacks {
-//            override fun onGetLink(link: String) {
-//                txt2.text = link
-//            }
-//
-//            override fun onError(error: String) {
-//                txt2.text = "Deu pau"
-//            }
-//        })
+        paymentsLink.generateLink(parameters2, object :
+            CieloPaymentsLinkCallbacks {
+            override fun onGetLink(link: String) {
+                txt2.text = link
+            }
+
+            override fun onError(error: String) {
+                txt2.text = "Deu pau $error"
+            }
+        })
 
     }
 }
